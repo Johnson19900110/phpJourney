d7da0c230a203ea9c718fb737082cb1fb0b6434e
Johnson <2534426042@qq.com>
2018-02-11 17:58:57 +0800

    test

--- app/Http/Controllers/App/HomeController.php
+++ app/Http/Controllers/App/HomeController.php
@@ -37,7 +37,7 @@ class HomeController extends Controller
                 'table' => 'users',
                 'type' => 'first'
             );
-
+dd($params);
             $data = $this->mysqlLink($params);
 
             dd($data);
