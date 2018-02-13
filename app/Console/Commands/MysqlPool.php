6222dbb0173cb433fe41f89040e8b9259237d959
Johnson <2534426042@qq.com>
2018-02-13 15:32:08 +0800

    test

--- app/Console/Commands/MysqlPool.php
+++ app/Console/Commands/MysqlPool.php
@@ -78,7 +78,8 @@ class MysqlPool extends Command
     {
         //taskwait就是投递一条任务，这里直接传递SQL语句了
         //然后阻塞等待SQL完成
-        $result = $serv->taskwait("show tables");
+        echo $data;
+        $result = $serv->taskwait($data);
         if ($result !== false) {
             list($status, $db_res) = explode(':', $result, 2);
             if ($status == 'OK') {
@@ -93,11 +94,10 @@ class MysqlPool extends Command
         }
     }
 
-    public function onTask($serv, $task_id, $from_id, $sql)
+    public function onTask($serv, $task_id, $from_id, $data)
     {
         static $link = null;
         if ($link == null) {
-//            $link = mysqli_connect("47.94.11.137", "root", "108178", "gogs");
             $link = DB::connection('mysql');
             if (!$link) {
                 $link = null;
@@ -105,6 +105,10 @@ class MysqlPool extends Command
                 return;
             }
         }
+
+        $data = json_decode($data);
+        var_dump($data);
+
         $result = $link->table('users')->get();
         if (!$result) {
             $serv->finish("ER:Sql Query Failed!!");
@@ -117,113 +121,4 @@ class MysqlPool extends Command
     {
         echo "AsyncTask Finish:Connect.PID=" . posix_getpid() . PHP_EOL;
     }
-
-    /*
-    protected function mysqlPoolStart()
-    {
-        $server = new \swoole_server("0.0.0.0", 9508, SWOOLE_BASE, SWOOLE_SOCK_TCP);
-        $server->set(array(
-            'worker_num' => 100,
-            'task_worker_num' => 10, //MySQL连接的数量
-	    'log_file' => base_path('storage/logs/swoole_http.log'),
-        ));
-
-        $server->on('Start', array(
-            $this,
-            'onStart'
-        ));
-
-        $server->on('Receive', array(
-            $this,
-            'onStart'
-        ));
-
-        $server->on('Task', array(
-            $this,
-            'onStart'
-        ));
-
-        $server->on('Finish', array(
-            $this,
-            'onStart'
-        ));
-
-        $server->start();
-    }
-
-    public function onStart($serv)
-    {
-        // 设置进程名称
-        echo 'Hello ' . $serv->master_pid. PHP_EOL;
-        cli_set_process_title("mysql_pool");
-    }
-
-    public function onReceive($server, $fd, $from_id, $data)
-    {
-        //taskwait can deliver one task
-        // Then block and wait for the task to complete
-        echo serialize($data) . PHP_EOL;
-        $result = $server->taskwait(json_decode($data));
-        if ($result !== false) {
-            list($status, $db_res) = explode(':', $result, 2);
-            if ($status == 'OK') {
-                //数据库操作成功了，执行业务逻辑代码，这里就自动释放掉MySQL连接的占用
-                $server->send($fd, var_export(unserialize($db_res), true) . "\n");
-            } else {
-                $server->send($fd, $db_res);
-            }
-            return;
-        } else {
-            $server->send($fd, "Error. Task timeout\n");
-        }
-    }
-
-    public function onTask($server, $task_id, $from_id, $data)
-    {
-	echo "task $task_id" . PHP_EOL;
-        static $link = null;
-        if ($link == null) {
-            $link = DB::connection('mysql');
-            if (!$link) {
-                $link = null;
-                $server->finish("ER:" . mysqli_error($link));
-                return;
-            }
-        }
-
-        if(!$table = $data['table']) {
-            $server->finish("ER:" . serialize('Be lacking of table'));
-            return;
-        }
-
-        if(!$queryType = $data['type']) {
-            $server->finish("ER:" . serialize('Be lacking of table'));
-            return;
-        }
-
-        $query = $link->table($table);
-
-        switch ($queryType) {
-            case 'find':
-                $result = $query->find($data['find']['id']);break;
-            case 'first':
-                $result = $query->first();break;
-            case 'get':
-            default:
-                $result = $query->get();
-        }
-
-        if (!$result) {
-            $server->finish("ER:" . mysqli_error($link));
-            return;
-        }
-
-        $server->finish("OK:" . serialize($data));
-    }
-
-    public function onFinish($serv, $data)
-    {
-        Log::info("AsyncTask Finish:Connect.PID=" . posix_getpid() . PHP_EOL);
-    }
-    */
 }
