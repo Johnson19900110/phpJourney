f5b07740c06fbd1da8ec5e329a7957c9765dd7d1
Johnson <2534426042@qq.com>
2018-02-14 10:19:05 +0800

    test

--- resources/views/widgets/navigation.blade.php
+++ resources/views/widgets/navigation.blade.php
@@ -1,4 +1,6 @@
 
-@foreach($categories as $category)
-    <li><a href="{{ route('category', $category->id) }}">{{ $category->name }}</a></li>
-@endforeach
+@if(isset($categories))
+    @foreach($categories as $category)
+        <li><a href="{{ route('category', $category->id) }}">{{ $category->name }}</a></li>
+    @endforeach
+@endif
